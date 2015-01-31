d4f0852bdcc1734b231b8e0ade83458d17bd3c88
pariskshitdutt <pdutt111>
2015-01-31 21:50:43 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var server;
+var app = express();
+var port = 1337;
+var data;
+var nodemailer = require("nodemailer");
+server = require('http').createServer(app)
+server.listen(port);
+
+app.configure(function(){
+        app.set("view options", { layout: false, pretty: true });
+        app.engine('html', require('ejs').renderFile);
+        app.set('views', __dirname + '/views');
+        app.use(express.favicon());
+        app.use("/public", express.static(__dirname + '/public'));
+    }
+);
+
+app.get('/', function(req, resp){
+    resp.render('index.html');
+});
+app.post('/mail',function(req,resp){
+     console.log(req.query);
+
+// create reusable transport method (opens pool of SMTP connections)
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+        service: "Gmail",
+        auth: {
+            user: "pdutt111@@gmail.com",
+            pass: "inception11"
+        }
+    });
+
+// setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: "pdutt111@gmail.com",
+        to: "pdutt111@gmail.com",
+        subject: "form fill mail",
+        text: "name: "+req.query.name+"\nemail: "+req.query.email+"\nmessage: "+req.query.message
+    }
+
+// send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        // if you don't want to use this transport object anymore, uncomment following line
+        smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+});
+
